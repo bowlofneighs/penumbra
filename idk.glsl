fd37124d195349806a5a72693527ab58451ae861
bowlofneighs <bowlofneighs@gmail.com>
2025-09-20 23:46:35 -0400

    added eyes to the circle, stopped because he started talking about color and its 11pm

--- idk.glsl
+++ idk.glsl
@@ -1,27 +1,22 @@
-vec3 palette( float t )
-{
-    vec3 a = vec3(0.5, 0.5, 0.5);
-    vec3 b = vec3(0.5, 0.5, 0.5);
-    vec3 c = vec3(3., 3., 3.);
-    vec3 d = vec3(0., 0.33, .66);
-    return a + b*cos(6.28318 * (c * t + d));
+float Circle(vec2 uv, vec2 p, float r, float blur){
+    float d = length(uv - p);
+    float c = smoothstep(r, r - blur, d);
+
+    return c;
 }
 
+
+
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord) {
-    vec3 finalColor = vec3(0.0);
-    vec2 uv = (fragCoord * 2.0 - iResolution.xy) /iResolution.y;
-    vec2 uv0 = uv;
+    vec2 uv = (fragCoord.xy / iResolution.xy); // normalize x and y coordinates
+    uv -= 0.5; // remaps uv from 0 <> 1.0 to -0.5 <> -0.5
+    uv.x *= iResolution.x/iResolution.y;
 
-    for (float i = 0.0; i < 3.0; i++){
-    uv = fract(uv*1.5) - 0.5;
-    float d = length(uv);
+    float c  = Circle(uv, vec2(.0 + .0), 0.4, 0.1);
 
-        vec3 col = palette(length(uv0) + iTime/4.);
+    c -= Circle(uv, vec2(.1, 0.1), .075, .005);
+    c -= Circle(uv, vec2(-.1, 0.1), 0.075, .005);
 
-    d = sin(d*8.0 + (iTime * 2.0))/8.0;
-    d = abs(d);
-    d = 0.04 / d;
-     finalColor += col * d/4.0;
-    }
-    fragColor = vec4(finalColor, 1.0);    
+    fragColor = vec4(vec3(c), 1.0);  
 }
