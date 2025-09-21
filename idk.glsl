4863de8abbf8d866c6c78a3dfa962cc8b0abb032
bowlofneighs <bowlofneighs@gmail.com>
2025-09-21 15:19:29 -0400

    made the smiley its own function, screen is black because the function is commented out

--- idk.glsl
+++ idk.glsl
@@ -6,15 +6,9 @@ float Circle(vec2 uv, vec2 p, float r, float blur){
 }
 
 
-
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord) {
-    vec2 uv = (fragCoord.xy / iResolution.xy); // normalize x and y coordinates
-    uv -= 0.5; // remaps uv from 0 <> 1.0 to -0.5 <> -0.5
-    uv.x *= iResolution.x/iResolution.y;
-
-    vec3 col = vec3(0.0);
-
+float Smiley(vec2 uv, vec2 p, float size) {
+    uv -= p;
+    uv /= size;
     float mask  = Circle(uv, vec2(.0 + .0), 0.4, 0.002);
 
     mask -= Circle(uv, vec2(.1, 0.2), .075, .002);
@@ -24,7 +18,22 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord) {
     mouth -= Circle(uv, vec2(.0,  .1), 0.3, 0.002);
     
     mask -= mouth;
-    //col = vec3(mouth);
+
+    return mask;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord) {
+
+    
+    vec2 uv = (fragCoord.xy / iResolution.xy); // normalize x and y coordinates
+    uv -= 0.5; // remaps uv from 0 <> 1.0 to -0.5 <> -0.5
+    uv.x *= iResolution.x/iResolution.y;
+
+    vec3 col = vec3(0.0);
+
+   float mask = 0.;
+   //Smiley(uv, vec2(0.), 1.);
+
     col = vec3(1.,1.,0)*mask;
     fragColor = vec4(col, 1.0);  
 }
